f4ea835dae790736551237c6e890b9d86042a0b2
2730538920101 <2730538920101@ingenieria.usac.edu.gt>
2024-06-10 22:58:10 -0600

    [ADD] Creación de base de datos e inserción en una tabla

---
+++ Practica1/db_init.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE PRACTICA1;
+
+USE PRACTICA1;
+
+-- Creación de la tabla PACIENTE
+CREATE TABLE PACIENTE(
+    idPaciente INT PRIMARY KEY,  -- Añadido AUTO_INCREMENT
+    edad INT,
+    genero VARCHAR(20)
+);
+
+-- Creación de la tabla HABITACION
+CREATE TABLE HABITACION(
+    idHabitacion INT PRIMARY KEY,  -- Añadido AUTO_INCREMENT
+    habitacion VARCHAR(50)
+);
+
+-- Creación de la tabla LOG_HABITACION
+CREATE TABLE LOG_HABITACION(
+    id_log_habitacion INT AUTO_INCREMENT PRIMARY KEY,
+    idHabitacion INT,  -- Definido solo el campo
+    timestampx DATETIME,  -- Cambiado a DATETIME
+    statusx VARCHAR(45),
+    FOREIGN KEY (idHabitacion) REFERENCES HABITACION(idHabitacion)  -- Definición de la clave externa
+);
+
+-- Creación de la tabla LOG_ACTIVIDAD
+CREATE TABLE LOG_ACTIVIDAD(
+    id_log_actividad INT AUTO_INCREMENT PRIMARY KEY,
+    timestampx DATETIME,  -- Cambiado a DATETIME
+    actividad VARCHAR(500),
+    idPaciente INT,
+    idHabitacion INT,
+    FOREIGN KEY (idPaciente) REFERENCES PACIENTE(idPaciente),  -- Definición de la clave externa
+    FOREIGN KEY (idHabitacion) REFERENCES HABITACION(idHabitacion)  -- Definición de la clave externa
+);
